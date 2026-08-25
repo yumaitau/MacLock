25a835462777dddafeb30e03d1ad115f2bd76135
Josh Luongo <josh@yumait.com.au>
2026-08-25 11:56:42 +1000

    Initial Commit

---
+++ MacLock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MacLock
+//
+//  Created by Josh Luongo on 25/8/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MacLock/MacLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLockApp.swift
+//  MacLock
+//
+//  Created by Josh Luongo on 25/8/2026.
+//
+
+import SwiftUI
+
+@main
+struct MacLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
